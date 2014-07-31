3fbe8c818ee4c0161ae9f14b1121c9eb8d934524
cgruber <cgruber@google.com>
2014-07-31 12:27:10 -0700

    Update Truth package name to the new location.
    Tested:
    TAP -p dagger,dagger_users #only tests are changed
    []
    Cloned from CL 72333859 by 'g4 patch'.
    Original change by kak@kak:rosie72333242-0189_Rosie:14707:citc on 2014/07/31 10:57:33.
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=72342044

--- compiler/src/test/java/dagger/internal/codegen/writer/TypeNamesTest.java
+++ compiler/src/test/java/dagger/internal/codegen/writer/TypeNamesTest.java
@@ -12,7 +12,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import static org.truth0.Truth.ASSERT;
+import static com.google.common.truth.Truth.ASSERT;
 
 @RunWith(JUnit4.class)
 public class TypeNamesTest {
