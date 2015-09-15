62a8b20e38fab093f94be1e96a98040935103a29
Christian Stein <sormuras@gmail.com>
2015-09-15 20:02:15 +0200

    Removed not need import statements.

--- src/main/java/com/squareup/javapoet/TypeName.java
+++ src/main/java/com/squareup/javapoet/TypeName.java
@@ -22,7 +22,6 @@ import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
 import java.lang.reflect.WildcardType;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
