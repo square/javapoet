96016ebc8482e83d91b38b377b8fd60e866f3a5f
Christian Stein <sormuras@gmail.com>
2015-10-07 06:22:23 +0200

    Added test for static methods in an interface.

--- src/test/java/com/squareup/javapoet/TypeSpecTest.java
+++ src/test/java/com/squareup/javapoet/TypeSpecTest.java
@@ -756,6 +756,28 @@ public final class TypeSpecTest {
   }
 
   @Test
+  public void interfaceStaticMethods() throws Exception {
+    assumeTrue(isJava8());
+    TypeSpec bar = TypeSpec.interfaceBuilder("Tacos")
+        .addMethod(MethodSpec.methodBuilder("test")
+            .addModifiers(Modifier.PUBLIC, Modifier.valueOf("STATIC"))
+            .returns(int.class)
+            .addCode(CodeBlock.builder().addStatement("return 0").build())
+            .build())
+        .build();
+
+    assertThat(toString(bar)).isEqualTo(""
+            + "package com.squareup.tacos;\n"
+            + "\n"
+            + "interface Tacos {\n"
+            + "  static int test() {\n"
+            + "    return 0;\n"
+            + "  }\n"
+            + "}\n"
+    );
+  }
+
+  @Test
   public void interfaceDefaultMethods() throws Exception {
     assumeTrue(isJava8());
     TypeSpec bar = TypeSpec.interfaceBuilder("Tacos")
