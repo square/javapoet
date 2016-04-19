2457d47f3441b06b21dd63996024de6c0fd924b4
marcosb <marcos+github@yuniti.com>
2016-04-19 12:32:46 -0600

    Update AnnotationSpecTest.java

--- src/test/java/com/squareup/javapoet/AnnotationSpecTest.java
+++ src/test/java/com/squareup/javapoet/AnnotationSpecTest.java
@@ -23,9 +23,12 @@ import java.lang.annotation.RetentionPolicy;
 import javax.lang.model.element.TypeElement;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import static com.google.common.truth.Truth.assertThat;
 
+@RunWith(JUnit4.class)
 public final class AnnotationSpecTest {
 
   @Retention(RetentionPolicy.RUNTIME)
