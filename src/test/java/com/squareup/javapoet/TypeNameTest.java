fc7a54553954c5df7852c7b0ce5fe49ee17dd100
marcosb <marcos+github@yuniti.com>
2016-04-20 11:56:22 -0600

    Update TypeNameTest.java

--- src/test/java/com/squareup/javapoet/TypeNameTest.java
+++ src/test/java/com/squareup/javapoet/TypeNameTest.java
@@ -22,14 +22,11 @@ import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 
 import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
-@RunWith(JUnit4.class)
 public class TypeNameTest {
 
   protected <E extends Enum<E>> E generic(E[] values) {
