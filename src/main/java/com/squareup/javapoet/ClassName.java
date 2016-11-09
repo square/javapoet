f0d6383c058b0fb0a2631d63768d52edef28da4b
Kay Roepke <kroepke@googlemail.com>
2016-11-09 13:03:30 +0100

    fix automatic import rearrangement

--- src/main/java/com/squareup/javapoet/ClassName.java
+++ src/main/java/com/squareup/javapoet/ClassName.java
@@ -20,7 +20,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
--- src/test/java/com/squareup/javapoet/ClassNameTest.java
+++ src/test/java/com/squareup/javapoet/ClassNameTest.java
@@ -16,16 +16,13 @@
 package com.squareup.javapoet;
 
 import com.google.testing.compile.CompilationRule;
-
+import java.util.Map;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.Elements;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-
-import java.util.Map;
-
-import javax.lang.model.element.TypeElement;
-import javax.lang.model.util.Elements;
 
 import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.assertEquals;
