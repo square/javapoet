825fc048bb2bc9c785a1e2ca3cd37baf919f6572
Tim Davenport <timothy.davenport@hsntech.com>
2017-08-31 13:15:17 +1000

    formatted Signed-off-by:Tim Davenport <timothy.davenport@hsntech.com>

--- src/main/java/com/squareup/javapoet/AnnotationSpec.java
+++ src/main/java/com/squareup/javapoet/AnnotationSpec.java
@@ -27,7 +27,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.AnnotationValue;
 import javax.lang.model.element.ExecutableElement;
@@ -42,19 +41,19 @@ import static com.squareup.javapoet.Util.checkNotNull;
 /** A generated annotation on a declaration. */
 public final class AnnotationSpec {
   public final TypeName type;
-    public final Map<String, List<CodeBlock>> members;
-    private boolean inlineMembers = false;
+  public final Map<String, List<CodeBlock>> members;
+  private boolean inlineMembers = false;
 
 
   private AnnotationSpec(Builder builder) {
     this.type = builder.type;
-      this.members = Util.immutableMultimap(builder.members);
-      this.inlineMembers = builder.inlineMembers;
+    this.members = Util.immutableMultimap(builder.members);
+    this.inlineMembers = builder.inlineMembers;
   }
 
   void emit(CodeWriter codeWriter, boolean inline) throws IOException {
-      String whitespace = inlineMembers ? "" : "\n";
-      String memberSeparator = inlineMembers ? ", " : ",\n";
+    String whitespace = inlineMembers ? "" : "\n";
+    String memberSeparator = inlineMembers ? ", " : ",\n";
     if (members.isEmpty()) {
       // @Singleton
       codeWriter.emit("@$T", type);
@@ -199,17 +198,17 @@ public final class AnnotationSpec {
 
   public static final class Builder {
     private final TypeName type;
-      private final Map<String, List<CodeBlock>> members = new LinkedHashMap<>();
-      private boolean inlineMembers = false;
+    private final Map<String, List<CodeBlock>> members = new LinkedHashMap<>();
+    private boolean inlineMembers = false;
 
     private Builder(TypeName type) {
       this.type = type;
-      }
+    }
 
-      public Builder inlineMembers(boolean inline) {
-          this.inlineMembers = inline;
-          return this;
-      }
+    public Builder inlineMembers(boolean inline) {
+        this.inlineMembers = inline;
+        return this;
+    }
 
     public Builder addMember(String name, String format, Object... args) {
       return addMember(name, CodeBlock.of(format, args));
