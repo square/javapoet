bf54fd38d3a4bc312c94092ea131023791c8e379
shaishavgandhi05 <shaishgandhi@gmail.com>
2018-10-07 14:05:56 -0700

    Add checks to ParameterSpec with VariableElement + copy over annotations
    Signed-off-by: shaishavgandhi05 <shaishgandhi@gmail.com>

--- src/test/java/com/squareup/javapoet/MethodSpecTest.java
+++ src/test/java/com/squareup/javapoet/MethodSpecTest.java
@@ -154,8 +154,8 @@ public final class MethodSpecTest {
         + "@java.lang.Override\n"
         + "protected <T extends java.lang.Runnable & java.io.Closeable> java.lang.Runnable "
         + "everything(\n"
-        + "    java.lang.String arg0, java.util.List<? extends T> arg1) throws java.io.IOException,\n"
-        + "    java.lang.SecurityException {\n"
+        + "    @com.squareup.javapoet.MethodSpecTest.Nullable java.lang.String arg0,\n"
+        + "    java.util.List<? extends T> arg1) throws java.io.IOException, java.lang.SecurityException {\n"
         + "}\n");
   }
 
