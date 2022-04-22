f40d5cd754045cde8d7ea33f54ebdb601cfb8f94
coro <honghongon@gmail.com>
2022-04-22 20:44:20 +0800

    add Record Class

--- src/test/java/RecordExample.java
+++ src/test/java/RecordExample.java
@@ -8,21 +8,36 @@ import java.io.IOException;
 
 public class RecordExample {
     public static void main(String[] args) throws IOException {
-        ClassName a = ClassName.get("com","a");
         TypeSpec record = TypeSpec.recordBuilder("Employee")
                 .addModifiers(Modifier.PUBLIC)
                 .addField(int.class, "empToken", Modifier.STATIC)
                 .addMethod(MethodSpec.CompactConstructorBuilder()
                         .addCode("if (id < 100) {\n" +
-                                "            throw new IllegalArgumentException(\n" +
-                                "                \"Employee Id cannot be below 100.\");\n" +
-                                "        }\n" +
-                                "        if (firstName.length() < 2) {\n" +
-                                "            throw new IllegalArgumentException(\n" +
-                                "                \"First name must be 2 characters or more.\");\n" +
-                                "        }")
+                                "    throw new IllegalArgumentException(" +
+                                "    \"Employee Id cannot be below 100.\");\n" +
+                                "}\n" +
+                                "if (firstName.length() < 2) {\n" +
+                                "    throw new IllegalArgumentException(\n" +
+                                "    \"First name must be 2 characters or more.\");\n" +
+                                "}")
                         .addModifiers(Modifier.PUBLIC).build())
-                .addMethod(MethodSpec.constructorBuilder().addParameter(int.class, "id").addParameter(String.class, "lastName").addStatement("this(id, firstName, null)").build())
+                .addMethod(MethodSpec.constructorBuilder()
+                        .addParameter(int.class, "id")
+                        .addParameter(String.class, "lastName")
+                        .addStatement("this(id, firstName, null)").build())
+                .addMethod(MethodSpec.methodBuilder("getFullName")
+                        .addCode("if (lastName == null)\n" +
+                                "    System.out.println(firstName());\n" +
+                                "\n" +
+                                "else\n" +
+                                "    System.out.println(firstName() + \" \"\n" +
+                                "                       + lastName());")
+                        .build())
+                .addMethod(MethodSpec.methodBuilder("generateEmployeeToken")
+                        .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                        .returns(int.class)
+                        .addCode("return ++empToken;")
+                        .build())
                 .addRecord(int.class, "id")
                 .addRecord(String.class, "firstName")
                 .addRecord(String.class, "lastName")
@@ -35,7 +50,7 @@ public class RecordExample {
                 .build();
 
 
-        JavaFile javaFile = JavaFile.builder("com.example.helloworld", record)
+        JavaFile javaFile = JavaFile.builder("com.example.record", record)
                 .build();
 
         javaFile.writeTo(System.out);
