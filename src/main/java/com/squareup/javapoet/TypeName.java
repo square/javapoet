2e650e2aa86f37bcf3f58977a158a7d29ea40c07
Yash <yashbest005@gmail.com>
2024-04-01 22:15:18 -0300

    add task 2 set 2 move method

--- src/main/java/com/squareup/javapoet/TypeName.java
+++ src/main/java/com/squareup/javapoet/TypeName.java
@@ -39,31 +39,45 @@ import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.SimpleTypeVisitor8;
 
 /**
- * Any type in Java's type system, plus {@code void}. This class is an identifier for primitive
- * types like {@code int} and raw reference types like {@code String} and {@code List}. It also
+ * Any type in Java's type system, plus {@code void}. This class is an
+ * identifier for primitive
+ * types like {@code int} and raw reference types like {@code String} and
+ * {@code List}. It also
  * identifies composite types like {@code char[]} and {@code Set<Long>}.
  *
- * <p>Type names are dumb identifiers only and do not model the values they name. For example, the
- * type name for {@code java.util.List} doesn't know about the {@code size()} method, the fact that
+ * <p>
+ * Type names are dumb identifiers only and do not model the values they name.
+ * For example, the
+ * type name for {@code java.util.List} doesn't know about the {@code size()}
+ * method, the fact that
  * lists are collections, or even that it accepts a single type parameter.
  *
- * <p>Instances of this class are immutable value objects that implement {@code equals()} and {@code
+ * <p>
+ * Instances of this class are immutable value objects that implement
+ * {@code equals()} and {@code
  * hashCode()} properly.
  *
  * <h3>Referencing existing types</h3>
  *
- * <p>Primitives and void are constants that you can reference directly: see {@link #INT}, {@link
+ * <p>
+ * Primitives and void are constants that you can reference directly: see
+ * {@link #INT}, {@link
  * #DOUBLE}, and {@link #VOID}.
  *
- * <p>In an annotation processor you can get a type name instance for a type mirror by calling
+ * <p>
+ * In an annotation processor you can get a type name instance for a type mirror
+ * by calling
  * {@link #get(TypeMirror)}. In reflection code, you can use {@link #get(Type)}.
  *
  * <h3>Defining new types</h3>
  *
- * <p>Create new reference types like {@code com.example.HelloWorld} with {@link
- * ClassName#get(String, String, String...)}. To build composite types like {@code char[]} and
+ * <p>
+ * Create new reference types like {@code com.example.HelloWorld} with {@link
+ * ClassName#get(String, String, String...)}. To build composite types like
+ * {@code char[]} and
  * {@code Set<Long>}, use the factory methods on {@link ArrayTypeName}, {@link
- * ParameterizedTypeName}, {@link TypeVariableName}, and {@link WildcardTypeName}.
+ * ParameterizedTypeName}, {@link TypeVariableName}, and
+ * {@link WildcardTypeName}.
  */
 public class TypeName {
   public static final TypeName VOID = new TypeName("void");
@@ -135,7 +149,8 @@ public class TypeName {
   }
 
   /**
-   * Returns true if this is a primitive type like {@code int}. Returns false for all other types
+   * Returns true if this is a primitive type like {@code int}. Returns false for
+   * all other types
    * types including boxed primitives and {@code void}.
    */
   public boolean isPrimitive() {
@@ -143,7 +158,8 @@ public class TypeName {
   }
 
   /**
-   * Returns true if this is a boxed primitive type like {@code Integer}. Returns false for all
+   * Returns true if this is a boxed primitive type like {@code Integer}. Returns
+   * false for all
    * other types types including unboxed primitives and {@code java.lang.Void}.
    */
   public boolean isBoxedPrimitive() {
@@ -159,60 +175,91 @@ public class TypeName {
   }
 
   /**
-   * Returns a boxed type if this is a primitive type (like {@code Integer} for {@code int}) or
+   * Returns a boxed type if this is a primitive type (like {@code Integer} for
+   * {@code int}) or
    * {@code void}. Returns this type if boxing doesn't apply.
    */
   public TypeName box() {
-    if (keyword == null) return this; // Doesn't need boxing.
+    if (keyword == null)
+      return this; // Doesn't need boxing.
     TypeName boxed = null;
-    if (keyword.equals(VOID.keyword)) boxed = BOXED_VOID;
-    else if (keyword.equals(BOOLEAN.keyword)) boxed = BOXED_BOOLEAN;
-    else if (keyword.equals(BYTE.keyword)) boxed = BOXED_BYTE;
-    else if (keyword.equals(SHORT.keyword)) boxed = BOXED_SHORT;
-    else if (keyword.equals(INT.keyword)) boxed = BOXED_INT;
-    else if (keyword.equals(LONG.keyword)) boxed = BOXED_LONG;
-    else if (keyword.equals(CHAR.keyword)) boxed = BOXED_CHAR;
-    else if (keyword.equals(FLOAT.keyword)) boxed = BOXED_FLOAT;
-    else if (keyword.equals(DOUBLE.keyword)) boxed = BOXED_DOUBLE;
-    else throw new AssertionError(keyword);
+    if (keyword.equals(VOID.keyword))
+      boxed = BOXED_VOID;
+    else if (keyword.equals(BOOLEAN.keyword))
+      boxed = BOXED_BOOLEAN;
+    else if (keyword.equals(BYTE.keyword))
+      boxed = BOXED_BYTE;
+    else if (keyword.equals(SHORT.keyword))
+      boxed = BOXED_SHORT;
+    else if (keyword.equals(INT.keyword))
+      boxed = BOXED_INT;
+    else if (keyword.equals(LONG.keyword))
+      boxed = BOXED_LONG;
+    else if (keyword.equals(CHAR.keyword))
+      boxed = BOXED_CHAR;
+    else if (keyword.equals(FLOAT.keyword))
+      boxed = BOXED_FLOAT;
+    else if (keyword.equals(DOUBLE.keyword))
+      boxed = BOXED_DOUBLE;
+    else
+      throw new AssertionError(keyword);
     return annotations.isEmpty() ? boxed : boxed.annotated(annotations);
   }
 
   /**
-   * Returns an unboxed type if this is a boxed primitive type (like {@code int} for {@code
+   * Returns an unboxed type if this is a boxed primitive type (like {@code int}
+   * for {@code
    * Integer}) or {@code Void}. Returns this type if it is already unboxed.
    *
-   * @throws UnsupportedOperationException if this type isn't eligible for unboxing.
+   * @throws UnsupportedOperationException if this type isn't eligible for
+   *                                       unboxing.
    */
   public TypeName unbox() {
-    if (keyword != null) return this; // Already unboxed.
+    if (keyword != null)
+      return this; // Already unboxed.
     TypeName thisWithoutAnnotations = withoutAnnotations();
     TypeName unboxed = null;
-    if (thisWithoutAnnotations.equals(BOXED_VOID)) unboxed = VOID;
-    else if (thisWithoutAnnotations.equals(BOXED_BOOLEAN)) unboxed = BOOLEAN;
-    else if (thisWithoutAnnotations.equals(BOXED_BYTE)) unboxed = BYTE;
-    else if (thisWithoutAnnotations.equals(BOXED_SHORT)) unboxed = SHORT;
-    else if (thisWithoutAnnotations.equals(BOXED_INT)) unboxed = INT;
-    else if (thisWithoutAnnotations.equals(BOXED_LONG)) unboxed = LONG;
-    else if (thisWithoutAnnotations.equals(BOXED_CHAR)) unboxed = CHAR;
-    else if (thisWithoutAnnotations.equals(BOXED_FLOAT)) unboxed = FLOAT;
-    else if (thisWithoutAnnotations.equals(BOXED_DOUBLE)) unboxed = DOUBLE;
-    else throw new UnsupportedOperationException("cannot unbox " + this);
+    if (thisWithoutAnnotations.equals(BOXED_VOID))
+      unboxed = VOID;
+    else if (thisWithoutAnnotations.equals(BOXED_BOOLEAN))
+      unboxed = BOOLEAN;
+    else if (thisWithoutAnnotations.equals(BOXED_BYTE))
+      unboxed = BYTE;
+    else if (thisWithoutAnnotations.equals(BOXED_SHORT))
+      unboxed = SHORT;
+    else if (thisWithoutAnnotations.equals(BOXED_INT))
+      unboxed = INT;
+    else if (thisWithoutAnnotations.equals(BOXED_LONG))
+      unboxed = LONG;
+    else if (thisWithoutAnnotations.equals(BOXED_CHAR))
+      unboxed = CHAR;
+    else if (thisWithoutAnnotations.equals(BOXED_FLOAT))
+      unboxed = FLOAT;
+    else if (thisWithoutAnnotations.equals(BOXED_DOUBLE))
+      unboxed = DOUBLE;
+    else
+      throw new UnsupportedOperationException("cannot unbox " + this);
     return annotations.isEmpty() ? unboxed : unboxed.annotated(annotations);
   }
 
-  @Override public final boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null) return false;
-    if (getClass() != o.getClass()) return false;
+  @Override
+  public final boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (o == null)
+      return false;
+    if (getClass() != o.getClass())
+      return false;
     return toString().equals(o.toString());
   }
 
-  @Override public final int hashCode() {
+  @Override
+  public final int hashCode() {
     return toString().hashCode();
   }
 
-  @Override public final String toString() {
+  @Override
+  public final String toString() {
     String result = cachedString;
     if (result == null) {
       try {
@@ -229,7 +276,8 @@ public class TypeName {
   }
 
   CodeWriter emit(CodeWriter out) throws IOException {
-    if (keyword == null) throw new AssertionError();
+    if (keyword == null)
+      throw new AssertionError();
 
     if (isAnnotated()) {
       out.emit("");
@@ -246,7 +294,6 @@ public class TypeName {
     return out;
   }
 
-
   /** Returns a type name equivalent to {@code mirror}. */
   public static TypeName get(TypeMirror mirror) {
     return get(mirror, new LinkedHashMap<>());
@@ -255,7 +302,8 @@ public class TypeName {
   static TypeName get(TypeMirror mirror,
       final Map<TypeParameterElement, TypeVariableName> typeVariables) {
     return mirror.accept(new SimpleTypeVisitor8<TypeName, Void>() {
-      @Override public TypeName visitPrimitive(PrimitiveType t, Void p) {
+      @Override
+      public TypeName visitPrimitive(PrimitiveType t, Void p) {
         switch (t.getKind()) {
           case BOOLEAN:
             return TypeName.BOOLEAN;
@@ -278,12 +326,12 @@ public class TypeName {
         }
       }
 
-      @Override public TypeName visitDeclared(DeclaredType t, Void p) {
+      @Override
+      public TypeName visitDeclared(DeclaredType t, Void p) {
         ClassName rawType = ClassName.get((TypeElement) t.asElement());
         TypeMirror enclosingType = t.getEnclosingType();
-        TypeName enclosing =
-            (enclosingType.getKind() != TypeKind.NONE)
-                    && !t.asElement().getModifiers().contains(Modifier.STATIC)
+        TypeName enclosing = (enclosingType.getKind() != TypeKind.NONE)
+            && !t.asElement().getModifiers().contains(Modifier.STATIC)
                 ? enclosingType.accept(this, null)
                 : null;
         if (t.getTypeArguments().isEmpty() && !(enclosing instanceof ParameterizedTypeName)) {
@@ -296,32 +344,39 @@ public class TypeName {
         }
         return enclosing instanceof ParameterizedTypeName
             ? ((ParameterizedTypeName) enclosing).nestedClass(
-            rawType.simpleName(), typeArgumentNames)
+                rawType.simpleName(), typeArgumentNames)
             : new ParameterizedTypeName(null, rawType, typeArgumentNames);
       }
 
-      @Override public TypeName visitError(ErrorType t, Void p) {
+      @Override
+      public TypeName visitError(ErrorType t, Void p) {
         return visitDeclared(t, p);
       }
 
-      @Override public ArrayTypeName visitArray(ArrayType t, Void p) {
+      @Override
+      public ArrayTypeName visitArray(ArrayType t, Void p) {
         return ArrayTypeName.get(t, typeVariables);
       }
 
-      @Override public TypeName visitTypeVariable(javax.lang.model.type.TypeVariable t, Void p) {
+      @Override
+      public TypeName visitTypeVariable(javax.lang.model.type.TypeVariable t, Void p) {
         return TypeVariableName.get(t, typeVariables);
       }
 
-      @Override public TypeName visitWildcard(javax.lang.model.type.WildcardType t, Void p) {
+      @Override
+      public TypeName visitWildcard(javax.lang.model.type.WildcardType t, Void p) {
         return WildcardTypeName.get(t, typeVariables);
       }
 
-      @Override public TypeName visitNoType(NoType t, Void p) {
-        if (t.getKind() == TypeKind.VOID) return TypeName.VOID;
+      @Override
+      public TypeName visitNoType(NoType t, Void p) {
+        if (t.getKind() == TypeKind.VOID)
+          return TypeName.VOID;
         return super.visitUnknown(t, p);
       }
 
-      @Override protected TypeName defaultAction(TypeMirror e, Void p) {
+      @Override
+      protected TypeName defaultAction(TypeMirror e, Void p) {
         throw new IllegalArgumentException("Unexpected type mirror: " + e);
       }
     }, null);
@@ -335,16 +390,26 @@ public class TypeName {
   static TypeName get(Type type, Map<Type, TypeVariableName> map) {
     if (type instanceof Class<?>) {
       Class<?> classType = (Class<?>) type;
-      if (type == void.class) return VOID;
-      if (type == boolean.class) return BOOLEAN;
-      if (type == byte.class) return BYTE;
-      if (type == short.class) return SHORT;
-      if (type == int.class) return INT;
-      if (type == long.class) return LONG;
-      if (type == char.class) return CHAR;
-      if (type == float.class) return FLOAT;
-      if (type == double.class) return DOUBLE;
-      if (classType.isArray()) return ArrayTypeName.of(get(classType.getComponentType(), map));
+      if (type == void.class)
+        return VOID;
+      if (type == boolean.class)
+        return BOOLEAN;
+      if (type == byte.class)
+        return BYTE;
+      if (type == short.class)
+        return SHORT;
+      if (type == int.class)
+        return INT;
+      if (type == long.class)
+        return LONG;
+      if (type == char.class)
+        return CHAR;
+      if (type == float.class)
+        return FLOAT;
+      if (type == double.class)
+        return DOUBLE;
+      if (classType.isArray())
+        return ArrayTypeName.of(get(classType.getComponentType(), map));
       return ClassName.get(classType);
 
     } else if (type instanceof ParameterizedType) {
@@ -377,18 +442,37 @@ public class TypeName {
     return result;
   }
 
-  /** Returns the array component of {@code type}, or null if {@code type} is not an array. */
+  /**
+   * Returns the array component of {@code type}, or null if {@code type} is not
+   * an array.
+   */
   static TypeName arrayComponent(TypeName type) {
     return type instanceof ArrayTypeName
         ? ((ArrayTypeName) type).componentType
         : null;
   }
 
-  /** Returns {@code type} as an array, or null if {@code type} is not an array. */
+  /**
+   * Returns {@code type} as an array, or null if {@code type} is not an array.
+   */
   static ArrayTypeName asArray(TypeName type) {
     return type instanceof ArrayTypeName
         ? ((ArrayTypeName) type)
         : null;
   }
 
+  // Move method
+
+  CodeWriter emitLeafType(CodeWriter writer) throws IOException {
+    ArrayTypeName arrayRepresentation = TypeName.asArray(this);
+    if (arrayRepresentation != null) {
+      TypeName elementType = arrayRepresentation.componentType;
+      if (elementType != null) {
+        // Handle variable arguments by outputting the element type without the array
+        // notation
+        return elementType.emitLeafType(writer);
+      }
+    }
+    return this.emit(writer);
+  }
 }
